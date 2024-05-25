0a373a399156b643494d13a3793e5ad16a53febf
murilinhops <murilinhorps@gmail.com>
2024-05-25 16:39:02 -0300

    Add background image

--- app/lib/app/(public)/bank_account/new_bank_account/new_bank_account_page.dart
+++ app/lib/app/(public)/bank_account/new_bank_account/new_bank_account_page.dart
@@ -46,108 +46,119 @@ class _NewBankAccountPageState extends State<NewBankAccountPage> {
             onPressed: () => Routefly.pop(context)),
         surfaceTintColor: Colors.transparent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 6),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const TextFieldInformation(text: 'Identificação do Banco*'),
-                TextFormField(
-                  //controller: controller.identificationController,
-                  decoration: InputDecoration(
-                    hintText: 'Insira o título para a solicitação',
-                    hintStyle: hintStyle,
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Identificação do Banco não pode estar vazio';
-                    }
-                    return null;
-                  },
-                ),
-                const TextFieldInformation(text: 'Agência*'),
-                TextFormField(
-                  // controller: controller.addressController,
-                  decoration: InputDecoration(
-                    hintText: 'Insira a agência bancária do Assistido',
-                    hintStyle: hintStyle,
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Não pode estar vazio';
-                    }
-                    return null;
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          const Positioned.fill(
+            child: Image(
+              image: CooImages.cooBackgroundDetails,
+              alignment: Alignment.bottomCenter,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const TextFieldInformation(text: 'Conta*'),
-                          TextFormField(
-                            // controller: controller.addressController,
-                            decoration: InputDecoration(
-                              hintText: 'Insira a Conta',
-                              hintStyle: hintStyle,
-                            ),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Não pode estar vazio';
-                              }
-                              return null;
-                            },
-                          ),
-                        ],
+                    const TextFieldInformation(text: 'Identificação do Banco*'),
+                    TextFormField(
+                      //controller: controller.identificationController,
+                      decoration: InputDecoration(
+                        hintText: 'Insira o título para a solicitação',
+                        hintStyle: hintStyle,
                       ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Identificação do Banco não pode estar vazio';
+                        }
+                        return null;
+                      },
                     ),
-                    const SizedBox(width: 8),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const TextFieldInformation(text: 'Digíto*'),
-                          TextFormField(
-                            //controller: controller.ufController,
-                            decoration: InputDecoration(
-                              hintText: 'Insira o Dígito',
-                              hintStyle: hintStyle,
-                            ),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Não pode estar vazia';
-                              }
-                              return null;
-                            },
-                          ),
-                        ],
+                    const TextFieldInformation(text: 'Agência*'),
+                    TextFormField(
+                      // controller: controller.addressController,
+                      decoration: InputDecoration(
+                        hintText: 'Insira a agência bancária do Assistido',
+                        hintStyle: hintStyle,
                       ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Não pode estar vazio';
+                        }
+                        return null;
+                      },
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const TextFieldInformation(text: 'Conta*'),
+                              TextFormField(
+                                // controller: controller.addressController,
+                                decoration: InputDecoration(
+                                  hintText: 'Insira a Conta',
+                                  hintStyle: hintStyle,
+                                ),
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Não pode estar vazio';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const TextFieldInformation(text: 'Digíto*'),
+                              TextFormField(
+                                //controller: controller.ufController,
+                                decoration: InputDecoration(
+                                  hintText: 'Insira o Dígito',
+                                  hintStyle: hintStyle,
+                                ),
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Não pode estar vazia';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const TextFieldInformation(text: 'Chave PIX da Conta*'),
+                    TextFormField(
+                      //controller: controller.ufController,
+                      decoration: InputDecoration(
+                        hintText: 'Insira a chave PIX do Assistido',
+                        hintStyle: hintStyle,
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Chave PIX não pode estar vazia';
+                        }
+                        return null;
+                      },
                     ),
                   ],
                 ),
-                const TextFieldInformation(text: 'Chave PIX da Conta*'),
-                TextFormField(
-                  //controller: controller.ufController,
-                  decoration: InputDecoration(
-                    hintText: 'Insira a chave PIX do Assistido',
-                    hintStyle: hintStyle,
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Chave PIX não pode estar vazia';
-                    }
-                    return null;
-                  },
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
       bottomNavigationBar: SafeArea(
         child: CooButton.primary(
